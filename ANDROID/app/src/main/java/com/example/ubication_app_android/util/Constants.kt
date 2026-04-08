83b12320817c304ac2cf0df8e14b410e08743872
JoseEM26 <joseangelespinozamorales@gmail.com>
2026-04-08 12:31:59 -0500

    migramos a railway 2

--- ANDROID/app/src/main/java/com/example/ubication_app_android/util/Constants.kt
+++ ANDROID/app/src/main/java/com/example/ubication_app_android/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.ubication_app_android.util
 
 object Constants {
-    const val BASE_URL = "http://57.156.64.242/api/"
+    const val BASE_URL = "https://asynchronous-production.up.railway.app/api/"
 }
--- asistencia_front/src/environments/environment.ts
+++ asistencia_front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://57.156.64.242/api'
+  apiUrl: 'https://asynchronous-production.up.railway.app/api'
 };
