38b580908a04bfcbbfb25ad0ca2a7211ca16cb66
JoseEM26 <joseangelespinozamorales@gmail.com>
2026-04-08 12:40:39 -0500

    migramos a railway 2

--- AplicacionMobil_IOS/src/main/java/com/asistenciaHibrida/AplicacionMobil_IOS/config/WebConfig.java
+++ AplicacionMobil_IOS/src/main/java/com/asistenciaHibrida/AplicacionMobil_IOS/config/WebConfig.java
@@ -1,20 +1,29 @@
 package com.asistenciaHibrida.AplicacionMobil_IOS.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "https://asistenciashibridas.up.railway.app",
-                        "http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList(
+                "https://asistenciashibridas.up.railway.app",
+                "http://localhost:4200"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
