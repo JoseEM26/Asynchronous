33e375af34e619119dcec54fb605a28f10afc5bc
williamch <william>
2026-04-26 13:07:29 -0500

    asdasd

--- ANDROID/app/src/main/java/com/example/ubication_app_android/data/model/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ubication_app_android.data.model
-
-data class LoginRequest(
-    val username: String,
-    val password: String
-)
--- ANDROID/app/src/main/java/com/example/ubication_app_android/util/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ubication_app_android.util
-
-object Constants {
-    const val BASE_URL = "https://asynchronous-production.up.railway.app/api/"
-}
